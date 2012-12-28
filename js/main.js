aebfadd94f589e1e487278aa6a90fd0a838d2c25
HairyFotr <hairyfotr@gmail.com>
2012-12-28 20:57:15 +0100

    finnaly sprites work

--- js/main.js
+++ js/main.js
@@ -6,71 +6,100 @@
 
     init();
         
-    function loadSprite(path) {
-        var img = THREE.ImageUtils.loadTexture(path + ".png");
-        var material = new THREE.SpriteMaterial({ map: img, useScreenCoordinates: false, color: 0xffffff });
-        
-        var sprite = new THREE.Sprite(material);
-        sprite.scale.set(img.image.width, img.image.height, 1);
-        sprite.width = img.image.width;
-        sprite.height = img.image.height;
-        
-        return sprite;
+    function loadSprite(path, callback, img) {
+        if(!img) {
+            var image = THREE.ImageUtils.loadTexture(path+'.png', {}, function(){ loadSprite(path, callback, image) });
+        } else {
+            var geometry = new THREE.PlaneGeometry(img.image.width, img.image.height);
+
+            var material = new THREE.MeshPhongMaterial({
+                map : img,
+                transparent : true
+            });
+            sprite = new THREE.Mesh(geometry, material);
+
+            /*var img = THREE.ImageUtils.loadTexture(path +".png");
+            var geometry = new THREE.PlaneGeometry(img.image.width, img.image.height);
+            var material = new THREE.MeshPhongMaterial({ 
+                map : img,
+                transparent : true
+            });
+            sprite = new THREE.Mesh(geometry, material);
+            //scene.add(world);*/
+
+
+            /*var img = THREE.ImageUtils.loadTexture(path + ".png");
+            var material = new THREE.SpriteMaterial({ map: img, useScreenCoordinates: false, color: 0xffffff });
+            var sprite = new THREE.Sprite(material);
+
+            sprite.scale.set(img.image.width, img.image.height, 1);*/
+
+            if(callback) callback(sprite)
+        }
     }
     
     function loadSprites(path, n) {
         var sprites = [];
         for(var i=0; i<n; i++) {
-            sprite = loadSprite(path + (i+1));
-            
-            sprites.push(sprite);
+            loadSprite(path + (i+1), function(sprite) {
+                sprites.push(sprite);
+            })
         }
-        
         return sprites;
-    }
-    
+    }    
     
     function animation(path,num,x,y,size) {
         that = this;
         this.x = x;
         this.y = y;
         this.size = size;
+        this.running = false;
         this.frame = 0;
         this.frames = num;
-        this.speed = 200;
+        this.speed = 100;
         this.frameTime = 0;
         this.sprites = loadSprites(path, that.frames);
-        this.update = function() {
-            var now = +new Date()
+        that.sprites.forEach(function(sprite) {
+            sprite.position.x = that.x
+            sprite.position.y = that.y
+            sprite.scale.x = sprite.scale.x*size
+            sprite.scale.y = sprite.scale.y*size
+        })
+        //this.width = that.sprites[0].material.map.image.width
+        //this.height = that.sprites[0].material.map.image.height
 
-            var lastFrame = that.frame
-            if(now > that.frameTime + that.speed) {
-                that.frame += 1
-                that.frame %= that.frames
-            }
-            
-            if(lastFrame != that.frame) {
-                scene.remove(that.sprites[lastFrame])
-                scene.add(that.sprites[that.frame])
-                that.frameTime = now
+        this.update = function() {
+            if(that.running) {
+                var now = +new Date()
+
+                var lastFrame = that.frame
+                if(now > that.frameTime + that.speed) {
+                    that.frame += 1
+                    that.frame %= that.frames
+                }
+                
+                if(lastFrame != that.frame && that.sprites[lastFrame] && that.sprites[that.frame]) {
+                    scene.remove(that.sprites[lastFrame])
+                    scene.add(that.sprites[that.frame])
+                    //that.sprites[that.frame].position.x = Math.random()*50-Math.random()*50
+                    //that.sprites[that.frame].position.y = Math.random()*50-Math.random()*50
+                    //that.sprites[that.frame].position.z = Math.random()*50-Math.random()*50
+                    that.frameTime = now
+                }
             }
         };
         this.start = function() {
-            that.sprites.forEach(function(sprite) {
-                sprite.position.x = that.x
-                sprite.position.y = that.y
-                sprite.scale.x = sprite.width*that.size
-                sprite.scale.y = sprite.height*that.size
-            })
+            that.running = true;
         };
         this.stop = function() {
+            that.running = false;
             that.sprites.forEach(function(sprite) {
                 scene.remove(sprite)
             })
         };
     }
     
-    function fire(x,y,size) { return new animation("img/fire",16,x,y,size) }
+    function fire(x,y,size) { return new animation("img/fire",16,x,y,size); }
 
     var prevTime = +new Date();
 
@@ -90,10 +119,8 @@
 
     }
     function initPlayer() {
-        var geometry = new THREE.PlaneGeometry(100, 200);
-        var texture = THREE.ImageUtils.loadTexture('img/player.png', {}, function() {
-            animate();
-        });
+        /*var geometry = new THREE.PlaneGeometry(100, 200);
+        var texture = THREE.ImageUtils.loadTexture('img/player.png');
         var material = new THREE.MeshPhongMaterial({
             map : texture,
             transparent : true
@@ -104,11 +131,20 @@
             x : 0,
             y : 0
         };
-        scene.add(player);
+        scene.add(player);*/
+        player = {}
+        player.jumpCount = 0;
+        player.speed = {
+            x : 0,
+            y : 0
+        };
+        loadSprite("img/player", function(sprite){ 
+            player.sprite = sprite
+            scene.add(player.sprite)
+        });
     }
 
    function init() {
-
         camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 1, 10000);
         camera.position.z = 1000;
 
@@ -127,7 +163,6 @@
         fire1.start();
 
         document.body.appendChild(renderer.domElement);
-
     }
 
     function update() {
@@ -154,23 +189,25 @@
             player.jumpLock = true;
         }
         player.speed.y -= 0.1 * dt;
-        player.position.x += player.speed.x * dt * 0.1;
-        player.position.y += player.speed.y;
-        if (player.position.y < -100) {
-            player.position.y = -100;
-            player.speed.y = 0;
-            player.jumpCount = 0;
-            player.jumpLock = false;
-            
+        if(player.sprite) {
+            player.sprite.position.x += player.speed.x * dt * 0.1;
+            player.sprite.position.y += player.speed.y;
+            if (player.sprite.position.y < -100) {
+                player.sprite.position.y = -100;
+                player.speed.y = 0;
+                player.jumpCount = 0;
+                player.jumpLock = false;
+                
+            }        
         }
-        
+
+        fire1.update();
     }
 
    function animate() {
 
         // note: three.js includes requestAnimationFrame shim
         update();
-        fire1.update();
 
         requestAnimationFrame(animate);
         renderer.render(scene, camera);
