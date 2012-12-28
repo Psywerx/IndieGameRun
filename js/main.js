a13d221d4c63c69eb1b2402096fa512bc05e1e45
Smotko <ensmotko@gmail.com>
2012-12-28 13:01:40 +0100

    Hackish fix of scrollbars issue

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 (function() {
+    var WIDTH = window.innerWidth-100, HEIGHT = window.innerHeight-100;
+
     var camera, scene, renderer;
     var geometry, material, mesh;
 
@@ -7,22 +9,25 @@
 
     function init() {
 
-        camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+        camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 1, 10000);
         camera.position.z = 1000;
 
         scene = new THREE.Scene();
 
         geometry = new THREE.CubeGeometry(200, 200, 200);
-        material = new THREE.MeshBasicMaterial({
-            color : 0xff0000,
-            wireframe : true
+        material = new THREE.MeshPhongMaterial({
+            color : 0xff0000
         });
 
         mesh = new THREE.Mesh(geometry, material);
         scene.add(mesh);
 
+        var light = new THREE.PointLight(0xff0000);
+        light.position.set(10, 50, 130);
+        scene.add(light);
+
         renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setSize(WIDTH, HEIGHT);
 
         document.body.appendChild(renderer.domElement);
 
