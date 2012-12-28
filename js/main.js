ab9cdb7607b8c8f7fea787eb7c797054092f71a5
HairyFotr <hairyfotr@gmail.com>
2012-12-28 13:41:49 +0100

    opacity

--- js/main.js
+++ js/main.js
@@ -14,17 +14,25 @@
 
         scene = new THREE.Scene();
 
-        geometry = new THREE.CubeGeometry(200, 200, 200);
-        texture = THREE.ImageUtils.loadTexture('test.jpg', {}, function() {
+        geometry = new THREE.PlaneGeometry(800, 800);
+        texture = THREE.ImageUtils.loadTexture('img/test.png', {}, function() {
             animate();
         });
-        material = new THREE.MeshPhongMaterial({
+        material1 = new THREE.MeshPhongMaterial({
             map: texture
+        });
+        material2 = new THREE.MeshPhongMaterial({
+            map: texture,
+            transparent: true
         });
         
         
-        mesh = new THREE.Mesh(geometry, material);
-        scene.add(mesh);
+        mesh1 = new THREE.Mesh(geometry, material1);
+        mesh2 = new THREE.Mesh(geometry, material2);
+        mesh2.z += 10
+        
+        scene.add(mesh1);
+        scene.add(mesh2);
 
         var pointLight = new THREE.PointLight(0xff0000);
         pointLight.position.set(10, 50, 130);
@@ -44,8 +52,10 @@
         // note: three.js includes requestAnimationFrame shim
         requestAnimationFrame(animate);
 
-        mesh.rotation.x += 0.01;
-        mesh.rotation.y += 0.02;
+        material2.opacity = 1 + Math.sin(new Date().getTime() * .0025)
+
+        mesh1.rotation.z += 0.01;
+        //mesh.rotation.y += 0.02;
 
         renderer.render(scene, camera);
 
