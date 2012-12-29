cc733b2a66928eb177c28db4ed9ca3a84e9c4034
Miha Zidar <zidarsk8@gmail.com>
2012-12-29 21:25:46 +0100

    template stuff

---
+++ js/lev/lev1.js
@@ -0,0 +1,8 @@
+var grond = [{"y":-200, "x":0, "w":3000, "h":300, "depth":100, "texture" : "img/floor_dark.png"}];
+
+var cloud = [{"y":-200, "x":0, "w":3000, "h":300, "depth":0,  "texture" : "img/cloud1.png", "y_speed":-60}];
+
+var fire = [{"y":-200, "x":0, "w":3000, "h":300, "depth":0}];
+
+var tree = [{"y":-200, "x":0, "w":3000, "h":300, "depth":0}];
+
