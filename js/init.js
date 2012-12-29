bc53eae4a0d2d49890335c89867c8bbafd1e2e05
Smotko <ensmotko@gmail.com>
2012-12-29 21:22:53 +0100

    Added sounds

--- js/init.js
+++ js/init.js
@@ -1,3 +1,16 @@
-(function( THREE, _) {
-    window.GAME = {};
-})( THREE, _ );
+(function(THREE, _) {
+    window.GAME = {
+        Sounds : {
+            moving : new buzz.sound("sounds/move", {
+                formats : [ "ogg" ],
+                preload : true,
+                loop : false
+            }),
+            jump : new buzz.sound("sounds/jump", {
+                formats : [ "ogg" ],
+                preload : true,
+                loop : false
+            })
+        }
+    };
+})(THREE, _);
