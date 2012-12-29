9fd98c6a5e96aa3219c5c33297d80cb11fa2a3eb
HairyFotr <hairyfotr@gmail.com>
2012-12-29 01:12:53 +0100

    player animation, world as sprite

--- js/sprites.js
+++ js/sprites.js
@@ -2,14 +2,15 @@
     var Sprite = G.Sprites = {};
 
     _.extend(Sprite, {
-        loadSprites: function(path, count, callback) {
+        loadSprites: function(path, count, callback, doneCallback) {
             var sprites = [];
             for(var i = 0; i < count; i++) {
                 (function(j) {
                     Sprite.loadSprite(path + (j+1), function(sprite) {
+                        callback && callback(sprite);
                         sprites[j] = sprite;
-                        if ( count === _.compact(sprites).length) { // all loaded
-                            callback();
+                        if (count === _.compact(sprites).length) { // all loaded
+                            doneCallback();
                         }
                     });
                 })(i);
@@ -34,6 +35,7 @@
                 }),
                 sprite = new THREE.Mesh(geometry, material);
 
+            sprite.material = material;
             sprite.width = width;
             sprite.height = height;
             callback && callback(sprite);
