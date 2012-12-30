37616a53b6f3cbba8129339503753a7eb18ff353
Miha Zidar <zidarsk8@gmail.com>
2012-12-30 04:33:59 +0100

    updated sun file

--- js/sun.js
+++ js/sun.js
@@ -8,19 +8,28 @@
         alpha= 0, 
         omega= 0.01, 
         size= 50,
+        posx = 0,
+        posy = 0,
         sprite= none;
-        
+      
+   
+    
     function getX(){
-        return 100;
+        return x+r*Math.cos(alpha);
     }
-    
     function getY(){
-        return 100;
+        return y+r*Math.sin(alpha);
     }
-    
+    function calcPos(){
+        posx = getX();
+        posy = getY();
+    }
+        
     _.extend(Sun, {
         update : function (dt) {
-           
+           alpha = omenga*dt;
+           calcPos();
+           sprite.position.set(posx, posy, 0);
         },
         init : function(sunObject){
             x = sunObject.x;
@@ -29,9 +38,10 @@
             alpha = sunObject.alpha;
             omega = sunObject.omega;
             size = sunObject.size;
+            calcPos();
             
             sprite = Sprite.getSprite("sun", "PLANE", size, size);
-            sprite.position.set(tree.x, tree.y, tree.depth || 0);
+            sprite.position.set(posx, posy, 0);
             
         }
     });
